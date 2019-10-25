0254837410f1355a3d3ff2a89085aa036b1d7c26
willianfalbo <willianfalbo@gmail.com>
2019-10-24 23:05:52 -0300

    setting up multer

--- gobarber-api/src/routes.js
+++ gobarber-api/src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // routes without authentication
 routes.post('/users', UserController.store);
@@ -13,5 +16,9 @@ routes.post('/sessions', SessionController.store);
 // routes with authentication
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json();
+});
 
 export default routes;
