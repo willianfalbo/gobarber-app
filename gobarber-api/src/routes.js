e75294cbd525025eeb9ee7a20613b4627e405315
willianfalbo <willianfalbo@gmail.com>
2019-10-27 00:44:25 -0300

    added provider schedule list

--- gobarber-api/src/routes.js
+++ gobarber-api/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ProviderScheduleController from './app/controllers/ProviderScheduleController';
 import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers-schedule', ProviderScheduleController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
