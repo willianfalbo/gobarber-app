c2d13ad0b4532a84fe20b2018db574ac0b959d67
willianfalbo <willianfalbo@gmail.com>
2020-07-28 21:26:01 -0300

    created frontend app with signin page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStypes from './styles/global';
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStypes />
+  </>
+);
+
+export default App;
