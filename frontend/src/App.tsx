c4620cc86820276c72045066f7950c393875dfcd
willianfalbo <willianfalbo@gmail.com>
2020-07-29 02:46:27 -0300

    frontend: created register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GlobalStypes from './styles/global';
-import SignIn from './pages/SignIn';
+// import SignIn from './pages/SignIn';
+import SignIn from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
     <Content>
       <img src={logoImg} alt="Barbershop" />
       <form>
-        <h1>Sign In</h1>
-        <Input name="Email" icon={FiMail} placeholder="Email" />
+        <h1>Sign Up</h1>
+        <Input name="name" icon={FiUser} placeholder="Name" />
+        <Input name="email" icon={FiMail} placeholder="Email" />
         <Input
           name="Password"
           icon={FiLock}
           placeholder="Password"
           type="password"
         />
-        <Button type="submit">SignIn</Button>
-        <a href="#forgot-password">Forgot password?</a>
+        <Button type="submit">Register</Button>
       </form>
-      <a href="#create-account">
-        <FiLogIn />
-        Create an account
+      <a href="#sign-in">
+        <FiArrowLeft />
+        Go back to sign in.
       </a>
     </Content>
-    <Background />
   </Container>
 );
 
-export default SignIn;
+export default SignUp;
