b94e11f6123127317194ec1f7718a2601e5aa8dd
willianfalbo <willianfalbo@gmail.com>
2020-09-26 02:28:50 -0300

    frontend: improved input usability

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,4 +1,9 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
 
 import { Container } from './styles';
@@ -8,11 +13,33 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input {...rest} />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isDirty, setIsDirty] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsDirty(!!inputRef.current?.value);
+  }, []);
+
+  return (
+    <Container isFocused={isFocused} isDirty={isDirty}>
+      {Icon && <Icon size={20} />}
+      <input
+        ref={inputRef}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        {...rest}
+        autoComplete="off"
+      />
+    </Container>
+  );
+};
 
 export default Input;
