ddf12206e53441a48a96c04e0d402555c4c7b20c
willianfalbo <willianfalbo@gmail.com>
2020-09-30 02:08:40 -0300

    frontend: configured authenticated routes

---
+++ frontend/src/routes/Route.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Route as RouteDOM, RouteProps as RoutePropsDOM, Redirect } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+interface RouteProps extends RoutePropsDOM {
+  isLoggedIn?: boolean;
+  component: React.ComponentType;
+}
+
+const Routes: React.FC<RouteProps> = ({ isLoggedIn = false, component: Component, ...rest }) => {
+  const { user } = useAuth();
+
+  // check if user is LoggedIn
+  return (
+    <RouteDOM
+      {...rest}
+      render={({ location }) => {
+        return isLoggedIn === !!user ? (
+          <Component />
+        ) : (
+            <Redirect to={{ pathname: isLoggedIn ? '/' : '/dashboard', state: { from: location } }} />
+          );
+      }}
+    />
+  );
+};
+
+export default Routes;
